2d2bcb64aad15d46242226979227e0a527a96314
Narendra <narendra1192cs@gmail.com>
2025-03-30 21:41:58 +0100

    mjw-3:Add type script generator

--- src/main/java/com/mjw/mjwservice/common/model/dashboard/HolidayDashboard.java
+++ src/main/java/com/mjw/mjwservice/common/model/dashboard/HolidayDashboard.java
@@ -2,11 +2,13 @@ package com.mjw.mjwservice.common.model.dashboard;
 
 import com.mjw.mjwservice.common.model.Review;
 import com.mjw.mjwservice.common.model.dashboard.config.DashboardData;
+import com.mjw.mjwservice.common.utility.ClientModel;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
+@ClientModel
 public record HolidayDashboard(
         String heroImageUrl,
         List<DashboardData> topDestinations,
